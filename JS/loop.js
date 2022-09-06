e7bd7bca7b936343d926b069c6d3a41193414b35
Mfitumukiza Peter <61537587+Peter-Mfitumukiza@users.noreply.github.com>
2022-09-06 09:42:21 +0200

    loops and simple demo

---
+++ JS/loop.js
@@ -0,0 +1,21 @@
+// for loop
+// while
+
+// let number = 0;
+
+// for(let i = 0; i<=10; i++){
+//     console.log(i);
+//     console.log("Helloo");
+// }
+
+// Sum of numbers less than a 100
+
+let sum = 0;
+
+for(let i=5; i>=0; i++){
+    // sum = sum + i;
+    console.log(i);
+}
+
+console.log(sum);
+
